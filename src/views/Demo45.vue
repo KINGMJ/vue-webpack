2923ca257e6c8e0f6be7fe188ffa6dcacc3105fb
jerry.mei <328047478@qq.com>
2020-04-22 08:06:01 +0800

    add demo

--- src/views/Demo45.vue
+++ src/views/Demo45.vue
@@ -2,14 +2,13 @@
   <div>
     <p>{{a}}</p>
     <p>{{b.name}}</p>
+    <!-- <p v-for="(item,index) in lists" :key="index">{{item}}</p> -->
     <button @click="triggerUpdateFailed">触发更新失败</button>
     <button @click="triggerUpdateSucceed">触发更新成功</button>
   </div>
 </template>
 
 <script>
-import PopperJS from "element-ui/src/utils/popper";
-
 export default {
   name: "Demo45",
   data() {
@@ -18,19 +17,22 @@ export default {
       b: {
         name: "Jack"
       },
-      c: [1, 2, 3]
+      lists: ["a", "b", "c"]
     };
   },
   methods: {
     triggerUpdateFailed() {
       this.b.age = 12;
     },
-
     triggerUpdateSucceed() {
-      // this.$set(this.b, "age", 12);
-      this.b = Object.assign({}, this.b, { age: 12, sex: "male" });
+      //this.$set(this.b, "age", 12);
+      // this.b = Object.assign({}, this.b, { age: 12, sex: "male" });
+      this.$set(this.lists, 3, "d");
     }
   },
+
+  mounted() {},
+
   updated() {
     console.log("触发更新");
   }
